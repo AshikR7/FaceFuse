0331f874722020110c0af7532df71d0ebcfff345
Ashik Rajan <ashikrajan7@gmail.com>
2024-03-10 19:26:56 +0530

    Update views.py

--- face_dection/faceapp/views.py
+++ face_dection/faceapp/views.py
@@ -7,43 +7,12 @@ from PIL import Image, ImageDraw
 
 
 from .forms import *
-from .models import *
-import os
+
 
 # Create your views here.
 
 def index(request):
     return render(request,'index.html')
-
-# def compare_view(request):
-#     if request.method=='POST':
-#         a=compare_photos_forms(request.POST,request.FILES)
-#         if a.is_valid():
-#             first_pic=a.cleaned_data['firstPhoto']
-#             second_pic=a.cleaned_data['secondPhoto']
-#
-#             img_1=face_recognition.load_image_file(first_pic)
-#             img_2=face_recognition.load_image_file(second_pic)
-#
-#             img_1_encoding=face_recognition.face_encodings(img_1)[0]
-#             img_2_encoding=face_recognition.face_encodings(img_2)[0]
-#
-#             known_encodings=[
-#                 img_1_encoding,
-#                 img_2_encoding
-#             ]
-#             img_test=face_recognition.load_image_file(first_pic)
-#             img_test_encoding=face_recognition.face_encodings(img_test)[0]
-#             face_distances = face_recognition.face_distance(known_encodings, img_test_encoding)
-#             balance=1-face_distances[1]
-#             percentage=math.ceil((balance/1)*100)
-#             return render(request,'result.html',{'face_distances':percentage})
-#         else:
-#             return HttpResponse("something went wrong")
-#     return render(request,'compare.html')
-
-
-
 
 
 def compare_view(request):
